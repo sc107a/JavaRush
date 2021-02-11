420ba00f5f5c9f515dd4468c14231747c3ca7f8d
sc107a <54020101+sc107a@users.noreply.github.com>
2021-02-11 14:44:13 +0300

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,61 +1,72 @@
-package com.javarush.task.task13.task1326;
+package com.javarush.task.task16.task1607;
 
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 /* 
-Сортировка четных чисел из файла
+Horse Racing
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        // напишите тут ваш код
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String sourseFileName = reader.readLine();
-            ArrayList<Integer> arrayList = new ArrayList<>();
-            try {
-                FileInputStream fileInputStream = new FileInputStream(sourseFileName);
-                while (fileInputStream.available() > 0) {arrayList.add(fileInputStream.read());}
-                fileInputStream.close();
-            } catch (FileNotFoundException e) {System.out.println("Файл тю-тю");}
-            ArrayList<String> stringList = new ArrayList<>();
-            ArrayList<Character> charArray = new ArrayList<>();
-                for (int k = 0; k < arrayList.size(); k++) {
-                    Integer x = arrayList.get(k);
-                    if (x != 10 ) { charArray.add((char)arrayList.get(k).intValue());}
-                    else {
-                        String str="";
-                        for (int i=0; i<charArray.size(); i++){
-                            str = str.concat(charArray.get(i).toString());
-                        }
-                        stringList.add(str);
-                        charArray.clear();
-                    }
-                    if (k == arrayList.size()-1){
-                        String str="";
-                        for (int i=0; i<charArray.size(); i++){
-                            str = str.concat(charArray.get(i).toString());
-                        }
-                        stringList.add(str);
-                        charArray.clear();
-                    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = prepareHorsesAndStart(10);
+        while (calculateHorsesFinished(horses) != horses.size()) {
+        }
+    }
+
+    public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {
+        int finishedCount = 0;
+//напишите тут ваш код
+        for (Horse h : horses) {
+            if (h.isFinished()) {
+            }
+            else {
+                System.out.println("Waiting for "+h.getName());
+                h.join();
+            }
+            finishedCount++;
+        }
+//конец моего кода
+        return finishedCount;
+    }
+
+    public static List<Horse> prepareHorsesAndStart(int horseCount) {
+        List<Horse> horses = new ArrayList<>(horseCount);
+        String number;
+        for (int i = 1; i < horseCount + 1; i++) {
+            number = i < 10 ? ("0" + i) : "" + i;
+            horses.add(new Horse("Horse_" + number));
+        }
+
+        for (int i = 0; i < horseCount; i++) {
+            horses.get(i).start();
+        }
+        return horses;
+    }
+
+    public static class Horse extends Thread {
+
+        private boolean isFinished;
+
+        public Horse(String name) {
+            super(name);
+        }
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+
+        public void run() {
+            String s = "";
+            for (int i = 0; i < 1001; i++) {   // Delay
+                s += "" + i;
+                if (i == 1000) {
+                    s = " has finished the race!";
+                    System.out.println(getName() + s);
+                    isFinished = true;
                 }
-                //фильтруем пробелы и создаем список Integer
-                ArrayList<Integer> integerList = new ArrayList<>();
-                for (String s : stringList){if (!s.equals("")) integerList.add(Integer.parseInt(s));}
-                //отделяем нечетные от четных
-            ArrayList<Integer> sortedArray = new ArrayList<>();
-            for (Integer i : integerList){if (i%2==0) sortedArray.add(i);}
-            Collections.sort(sortedArray);
-            for (Integer i : sortedArray) System.out.println(i);
-
-
+            }
+        }
     }
 }
-
-
-
-
